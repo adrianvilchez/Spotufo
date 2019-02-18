1ec9b0c01012f1dbfce961b284d1984097fe59d7
adrianvilchez <avilchez758@gmail.com>
2019-02-18 02:10:47 +0100

    Adición de botones y modificación del script del scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,11 @@
 function myFunction() {
 
   if ($(".temazosChillScroll").scrollTop() > 290){
-
         $('.nombreCanciones').addClass("fixed").fadeIn();
+        $('.menuReproduccion').css('border-bottom', '1px solid #7B7B7B');
   }
   else {
-
         $('.nombreCanciones').removeClass("fixed");
+        $('.menuReproduccion').css('border-bottom', '0px solid');
   }
 }
